483df218f6d1a5e1cc1ce315023e7c973942f41a
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-05-30 09:11:10 -0300

    Consulta letra terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
+import Cancion from './components/Cancion';
 
 function App() {
+
+  // definir state para la consulta
+  const [ busquedaLetra, guardarBusquedaLetra ] = useState({});
+  // definir state para la letra
+  const [ letra, guardarLetra ] = useState('');
+
+  // consulta a la API
+  useEffect(()=>{
+    if (Object.keys(busquedaLetra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaLetra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const resultado = await axios(url);
+      guardarLetra(resultado.data.lyrics);
+    }
+    consultarApiLetra();
+  }, [busquedaLetra]);
+
   return (
     <Fragment>
       <Formulario 
+        guardarBusquedaLetra={guardarBusquedaLetra}
       />
+
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+            1
+          </div>
+          <div className='col-md-6'>
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+
     </Fragment>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
+
+    // definir state de busqueda
+    const [ busqueda, guardarBusqueda ] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    const { artista, cancion } = busqueda;
+    // funcion para leer el contenido de cada input
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    // definir state para el error
+    const [ error, guardarError ] = useState(false);
+
+    // consultar las APIS
+    const buscarInformacion = e => {
+        e.preventDefault();
+        if ( artista.trim() === '' || cancion.trim() === '' ) {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        // Validado, pasar al componente principal
+        guardarBusquedaLetra(busqueda);
+    }
+
     return ( 
-        <p>HOLA</p>
+        <div className='bg-info'>
+            <div className='container'>
+
+                <div className='row'>
+
+                    <form
+                        onSubmit={buscarInformacion}
+                        className='col card text-white bg-transparent mb-5 pt-5 pb-2'
+                    >
+                        <fieldset>
+                            <legend 
+                                className='text-center'
+                            >Buscador Letras Canciones</legend>
+                            { error 
+                                ? <p
+                                    className='alert alert-danger text-center p-2'
+                                    >Todos los campos son obligatorios</p>
+                                : null
+                            }
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <div className='form-group'>
+                                        <label>Artista</label>
+                                        <input
+                                            type='text'
+                                            className='form-control'
+                                            name='artista'
+                                            placeholder='Nombre Artista'
+                                            onChange={actualizarState}
+                                            value={artista}
+                                        />
+                                    </div>
+                                </div>
+                                <div className='col-md-6'>
+                                    <div className='form-group'>
+                                        <label>Canción</label>
+                                        <input
+                                            type='text'
+                                            className='form-control'
+                                            name='cancion'
+                                            placeholder='Nombre Canción'
+                                            onChange={actualizarState}
+                                            value={cancion}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button
+                                type='submit'
+                                className='btn btn-primary float-right'
+                            >Buscar</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
  
